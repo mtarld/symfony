d26840ff8df20e9b11ee9b3df07674476cf3f725
Mathias Arlaud <mathias.arlaud@gmail.com>
2022-11-04 16:35:44 +0100

    wip

--- src/Dto/Foo.php
+++ src/Dto/Foo.php
@@ -20,15 +20,53 @@ use Symfony\Component\Marshaller\Context\Option\GroupsOption;
 final class Foo
 {
     #[Name('@id')]
-    #[Groups(['groupOne', 'groupTwo'])]
     #[Formatter(self::class, 'iri')]
     public string $iri = 'theIri';
 
     #[Groups('groupTwo')]
     public ?int $price = 12;
 
-    // /** @var array<string, string|null>|null */
-    // public ?array $dict;
+    /** @var array<string, string|null>|null */
+    public ?array $dict;
+
+    /** @var array<string, string|null>|null */
+    #[Hook('my_property_accessor')]
+    public array $collection; // component
+
+    /** @var array<string, string|null>|null */
+    private array $collection;
+
+
+// [
+// 'cache_path' => '' ?? sys_tmp_dir(),
+// 'hooks' => {
+// 'array' => fn
+// 'Foo::$collection' => fn,
+// 'max_depth' => 512,
+// 'reject_circular_reference' => true,
+// }
+// ]
+
+
+    marshal($data, $resource, $format, $context = []): void;
+
+    json_marshal($data, $resource, $context): void;
+    json_generate(\Reflector $type, array $context): string;
+
+    xml_marshal($data, $resource, $context): void;
+    xml_generate(\Reflector $type, array $context): string;
+
+    compute_metadata(\Reflector $type, array $context): string;
+
+    my_hook(\Reflector $type, array $context): string;
+
+    #[CustomGenerator('phpstan_formatter')] // polyfill
+    public array $collection;
+
+    /** @return array<Stmt> */
+    phpstan_formatter(\ReflectionAttribte $attribute, array $context): array {
+        return [new Expr\...];
+    }
 
     /** @var list<string|null>|null */
     public ?array $list = null;
